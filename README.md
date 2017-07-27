1f226d4d0798f888f6ebbe710bac52fb24ec14cd
york yao <yaoao12306@outlook.com>
2017-07-27 21:32:20 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+[![Dependency Status](https://david-dm.org/plantain-00/clean-scripts.svg)](https://david-dm.org/plantain-00/clean-scripts)
+[![devDependency Status](https://david-dm.org/plantain-00/clean-scripts/dev-status.svg)](https://david-dm.org/plantain-00/clean-scripts#info=devDependencies)
+[![Build Status](https://travis-ci.org/plantain-00/clean-scripts.svg?branch=master)](https://travis-ci.org/plantain-00/clean-scripts)
+[![npm version](https://badge.fury.io/js/clean-scripts.svg)](https://badge.fury.io/js/clean-scripts)
+[![Downloads](https://img.shields.io/npm/dm/clean-scripts.svg)](https://www.npmjs.com/package/clean-scripts)
+
 # clean-scripts
 A CLI tool to make scripts in package.json clean.
+
+#### install
+
+`npm i clean-scripts -g`
+
+#### usage
+
+run `clean-scripts script-name`
+
+or run `clean-scripts script-name --config clean-scripts.config.js`
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import * as minimist from "minimist";
+import * as childProcess from "child_process";
+import * as path from "path";
+import * as packageJson from "../package.json";
+
+const defaultConfigName = "clean-scripts.config.js";
+
+function printInConsole(message: any) {
+    // tslint:disable-next-line:no-console
+    console.log(message);
+}
+
+function showToolVersion() {
+    printInConsole(`Version: ${packageJson.version}`);
+}
+
+async function executeCommandLine() {
+    const argv = minimist(process.argv.slice(2), { "--": true });
+
+    const showVersion = argv.v || argv.version;
+    if (showVersion) {
+        showToolVersion();
+        return;
+    }
+
+    const scripts: { [name: string]: string[] } = require(path.resolve(process.cwd(), argv.config || defaultConfigName));
+
+    const scriptNames = argv._;
+    for (const scriptName of scriptNames) {
+        const scriptValues = scripts[scriptName];
+        if (!scriptValues) {
+            throw new Error(`Unknown script name: ${scriptName}`);
+        }
+        for (const script of scriptValues) {
+            printInConsole(script);
+            const stdout = childProcess.execSync(script, { encoding: "utf8" });
+            printInConsole(stdout);
+        }
+    }
+}
+
+try {
+    executeCommandLine();
+    printInConsole("success.");
+} catch (error) {
+    printInConsole(error.stdout);
+    process.exit(error.status);
+}
