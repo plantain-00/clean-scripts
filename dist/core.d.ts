3bb8e2a4c52f081b2229afb27512f48c37aa6d59
york yao <yaoao12306@outlook.com>
2017-10-09 14:05:15 +0800

    publish declaration files

---
+++ dist/core.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="node" />
+/**
+ * @public
+ */
+export declare function sleep(ms: number): Promise<{}>;
+import * as stream from "stream";
+/**
+ * @public
+ */
+export declare function readableStreamEnd(readable: stream.Readable): Promise<void>;
+/**
+ * @public
+ */
+export declare class Service {
+    script: string;
+    constructor(script: string);
+}
